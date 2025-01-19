12960353b4142e0f5a2fff6b1b61925352f6728a
Stanislav <sk_689@mail.ru>
2025-01-19 18:53:55 +0300

    add task 3,4,5

--- src/main/java/ru/otus/java/basic/homeworks/homework3/AppHW3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/AppHW3.java
@@ -1,9 +1,14 @@
 package ru.otus.java.basic.homeworks.homework3;
 
+import ru.otus.java.basic.homeworks.Main;
+
 public class AppHW3 {
     public static void main(String[] args) {
 //        greetings();
 //        checkSing(-10, 5, 5);
+//        selectColor();
+//        compareNumbers();
+        addOrSubtractAndPrint(100,10,false);
     }
 
     public static void greetings() {
@@ -17,6 +22,36 @@ public class AppHW3 {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
+        }
+    }
+
+
+    public static void selectColor() {
+        int data = 1 + (int) (Math.random() * 30);
+        if (data <= 10) {
+            System.out.println("Красный");
+        } else if (data > 10 && data <= 20) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = (int) (Math.random() * 100);
+        int b = (int) (Math.random() * 100);
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
+        if (increment == true){
+            System.out.println(initValue + delta);
+        } else {
+            System.out.println(initValue - delta);
         }
     }
 
