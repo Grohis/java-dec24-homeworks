e07bc5e790038eb8f792144b6eded95ba67bb6ef
Stanislav <sk_689@mail.ru>
2025-01-30 17:39:12 +0300

    доработал принт

--- src/main/java/ru/otus/java/basic/homeworks/homework7/AppHW7.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework7/AppHW7.java
@@ -5,17 +5,21 @@ import java.util.Random;
 
 public class AppHW7 {
     public static void main(String[] args) {
-       // homeTask1();
-       // homeTask2();
-        homeTaskZeroDiagonalElements();
+        // homeTask1();
+        //homeTaskPrintArrayElements();
+        // homeTaskZeroDiagonalElements();
+
 
     }// main
 
     private static void homeTaskZeroDiagonalElements() {
-        int[][] zeroDiagonalElements = getDataDigit(3,3);
+        int[][] zeroDiagonalElements = getDataDigit(10, 10);
         for (int i = 0; i < zeroDiagonalElements.length; i++) {
             for (int j = 0; j < zeroDiagonalElements[i].length; j++) {
-                if(zeroDiagonalElements[i] == zeroDiagonalElements[j]){
+                if (i == j) {
+                    zeroDiagonalElements[i][j] = 0;
+                }
+                if (i + j == zeroDiagonalElements.length - 1) {
                     zeroDiagonalElements[i][j] = 0;
                 }
             }
@@ -23,13 +27,13 @@ public class AppHW7 {
         printArray(zeroDiagonalElements);
     }
 
-    private static void homeTask2() {
-        int size = 2;
+    private static void homeTaskPrintArrayElements() {
+        int size = 10;
         printSizeArray(size);
     }
 
     private static void printSizeArray(int size) {
-        char [][] sizeArray = new char[size][size];
+        char[][] sizeArray = new char[size][size];
         for (int i = 0; i < sizeArray.length; i++) {
             for (int j = 0; j < sizeArray[i].length; j++) {
                 sizeArray[i][j] = '*';
@@ -37,14 +41,14 @@ public class AppHW7 {
         }
         for (int i = 0; i < sizeArray.length; i++) {
             for (int j = 0; j < sizeArray[i].length; j++) {
-                System.out.print(sizeArray[i][j] + " ");
+                System.out.print(sizeArray[i][j] + "\t");
             }
             System.out.println();
         }
     }
 
     private static void homeTask1() {
-        int[][] integerData = getDataDigit(3,3);
+        int[][] integerData = getDataDigit(3, 3);
         printArray(integerData);
         int sumPositiveDigit = sumOfPositiveElements(integerData);
         System.out.println("Сумма положительных чисел двехмерного массива = " + sumPositiveDigit);
@@ -54,7 +58,7 @@ public class AppHW7 {
         int sumDigit = 0;
         for (int i = 0; i < dataDigit.length; i++) {
             for (int j = 0; j < dataDigit[i].length; j++) {
-                if (dataDigit[i][j] > 0){
+                if (dataDigit[i][j] > 0) {
                     sumDigit += dataDigit[i][j];
                 }
             }
@@ -66,16 +70,16 @@ public class AppHW7 {
         System.out.print("Двухмерный массива имеет следующие значения");
         System.out.println();
         for (int i = 0; i < dataDigit.length; i++) {
-            for (int j = 0; j < dataDigit[i].length ; j++) {
-                System.out.print(dataDigit[i][j] + " ");
+            for (int j = 0; j < dataDigit[i].length; j++) {
+                System.out.print(dataDigit[i][j] + "\t");
             }
             System.out.println();
         }
     }
 
     private static int[][] getDataDigit(int x, int y) {
-        int[][] dataDigit = new int [y][x];
-        for (int i = 0; i <dataDigit.length; i++){
+        int[][] dataDigit = new int[y][x];
+        for (int i = 0; i < dataDigit.length; i++) {
             for (int j = 0; j < dataDigit[i].length; j++) {
                 dataDigit[i][j] = getDigit();
             }
