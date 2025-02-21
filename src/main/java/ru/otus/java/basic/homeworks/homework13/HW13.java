c57cb19e56c202918d31c607ea5eed45709912a1
Stanislav <sk_689@mail.ru>
2025-02-21 07:41:15 +0300

    проверка циклом

--- src/main/java/ru/otus/java/basic/homeworks/homework13/HW13.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework13/HW13.java
@@ -8,14 +8,20 @@ public class HW13 {
     Bicycle bicycle = new Bicycle();
     AllTerrainVehicle atv = new AllTerrainVehicle(40);
 
-        human.move("равнина", 5);
-        human.sit(car);
-        human.move("густой лес", 10);
-        human.move("равнина", 20);
-        human.getOff();
-        human.sit(horse);
-        human.move("болото", 10);
-        human.move("равнина", 10);
-        human.getOff();
+        String[] terrains = {"равнина", "густой лес", "болото", "равнина"};
+        int[] distances = {10, 5, 10, 15};
+
+        Transport[] transports = {car, horse, bicycle, atv};
+
+        for (Transport transport : transports) {
+            human.sit(transport);
+            for (int i = 0; i < terrains.length; i++) {
+                if (!human.move(terrains[i], distances[i])) {
+                    System.out.println("Перемещение невозможно. Попробуем другой транспорт.");
+                    break;
+                }
+            }
+            human.getOff();
+        }
     }
 }
