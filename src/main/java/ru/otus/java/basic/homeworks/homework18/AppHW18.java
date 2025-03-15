4478a44519477c6b396b1e68435322d7c442f4d6
Stanislav <sk_689@mail.ru>
2025-03-15 14:08:53 +0300

    стабильна

--- src/main/java/ru/otus/java/basic/homeworks/homework18/AppHW18.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework18/AppHW18.java
@@ -1,20 +1,32 @@
 package ru.otus.java.basic.homeworks.homework18;
 
+import java.util.Arrays;
+
 public class AppHW18 {
     public static void main(String[] args) {
         PersonDataBase database = new PersonDataBase();
 
-        Person p1 = new Person("Alice", Position.MANAGER, 1L);
-        Person p2 = new Person("Bob", Position.DEVELOPER, 2L);
-        Person p3 = new Person("Charlie", Position.JANITOR, 3L);
+        Person p1 = new Person("Алиса", Position.MANAGER, 1L);
+        Person p2 = new Person("Иван", Position.DEVELOPER, 2L);
+        Person p3 = new Person("Оля", Position.JANITOR, 3L);
 
         database.add(p1);
         database.add(p2);
         database.add(p3);
 
-        System.out.println("Find by ID 2: " + database.findById(2L).name);
-        System.out.println("Is Alice a manager? " + database.isManager(p1));
-        System.out.println("Is Bob an employee? " + database.isEmployee(2L));
-        System.out.println("Is Charlie a manager? " + database.isManager(p3));
+        System.out.println("Найти по id 2: " + database.findById(2L).name);
+        System.out.println("Алиса менеджер? " + database.isManager(p1));
+        System.out.println("Иван сотрудник? " + database.isEmployee(2L));
+        System.out.println("Оля менеджер? " + database.isManager(p3));
+
+
+
+        int[] array = {5, 3, 8, 1, 2, 7};
+        Sorter.bubbleSort(array);
+        System.out.println("Сортировка пузырьком: " + Arrays.toString(array));
+
+        int[] array2 = {5, 3, 8, 1, 2, 7};
+        Sorter.quickSort(array2);
+        System.out.println("Быстрая сортировка: " + Arrays.toString(array2));
     }
 }
